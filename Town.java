beb4e2b941a10a5eb452ab1d3ee93fa733239c3c
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-20 00:37:41 +0300

    Town.java

---
+++ Town.java
@@ -0,0 +1,18 @@
+public class Town {
+
+    private String name;
+    private int distance;
+
+    public Town(String name, int distance){
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
